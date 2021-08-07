64c7b130731aa3313d9dc296b0db2d0ab7eb509d
Aylin Santa Cruz Vargas <aylin_santacruz@hotmail.com>
2021-08-06 23:51:23 -0500

    update

--- e-commerce/src/services/productosService.js
+++ e-commerce/src/services/productosService.js
@@ -12,6 +12,62 @@ const obtenerProductos = async () =>{
     }
 }
 
+const crearProducto = async (nuevoProducto) => {
+    try {
+        const headers = {
+            "Content-Type": "application/json"
+        }
+        let { data } = await axios.post(URL, nuevoProducto, {headers})
+        return data
+    } catch (error) {
+        throw error
+    }
+}
+
+const obtenerProductoPorId = async (id) => {
+    try {
+        let {data} = await axios.get(`${URL}/${id}`)
+        return data
+    } catch (error) {
+        throw MediaError
+    }
+}
+
+const editarProducto = async (productoEditado,id) =>{
+    try {
+        const headers = {
+            "Content-Type": "application/json"
+        }
+        let {data} = await axios.put(`${URL}/${id}`, productoEditado, {headers})
+        return data
+    } catch (error) {
+        throw error
+    }
+}
+
+
+const subirArchivo = (imagen) => {
+    return new Promise((resolve, reject) => {
+        let refStorage = storage.ref(`fotos/${imagen.name}`)
+        let tareaSubir = refStorage.put(imagen)
+
+        tareaSubir.on("state_changed",
+            () => {},
+            (error) => {reject(error)},
+            () => {
+                tareaSubir.snapshot.ref.getDownloadURL()
+                .then((urlImagen) => {
+                    resolve(urlImagen)
+                })
+            }
+        )
+    })
+}
+
 export{
-    obtenerProductos
+    obtenerProductos,
+    crearProducto,
+    obtenerProductoPorId,
+    editarProducto,
+    subirArchivo
 }
