ad5c926dc220069d1f6af3d45401f720ab04fa3c
Aylin Santa Cruz Vargas <aylin_santacruz@hotmail.com>
2021-08-10 23:46:32 -0500

    agregando catalogo

--- e-commerce/src/Routes.js
+++ e-commerce/src/Routes.js
@@ -2,6 +2,7 @@ import {Route} from "react-router-dom"
 import ListaProductosView from "./views/ListaProductosView"
 import CrearProductoView from "./views/CrearProductoView"
 import EditarProductoView from "./views/EditarProductoView"
+import ProductoView from "./views/ProductoView"
 
 export default function Routes() {
     return (
@@ -9,6 +10,7 @@ export default function Routes() {
             <Route path="/" exact component={ListaProductosView} />
             <Route path="/crear" exact component={CrearProductoView} />
             <Route path="/editar/:id" exact component={EditarProductoView} />
+            <Route path="/detalle/:id" exact component={ProductoView}/>
         </div>
     )
 }
